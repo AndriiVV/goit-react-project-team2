03345283bc1b482ff82cf1b37010e2f516f9a21f
Kateryna <k.borysenko.kyiv@gmail.com>
2022-04-07 01:39:04 +0300

    Added Langs switcher

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { App } from './components/App';
 import store from './redux/store';
 import './index.css';
+import './i18n'
 
 ReactDOM.render(
   <React.StrictMode>
