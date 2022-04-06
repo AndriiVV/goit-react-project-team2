d540db054530ae0ac338daa482b13bb22dd230c1
Oleksandr Yakob <yakoboleksandr@gmail.com>
2022-04-06 11:26:27 +0300

    docs: delete useless import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,6 @@
 import { lazy, Suspense } from 'react';
 import PrivateRoute from './PrivateRoute/PrivateRoute';
 import PublicRoute from './PublicRoute/PublicRoute';
-// import LoginPage from '../pages/LoginPage/LoginPage';
-// import RegisterPage from '../components/RegisterPage/RegisterPage';
-// import LibraryPage from '../pages/LibraryPage/LibraryPage';
-// import TrainingPage from '../pages/TrainingPage/TrainingPage';
 import MainNav from '../pages/MainNav';
 import { Route } from 'react-router-dom';
 import Container from './common/Container';
