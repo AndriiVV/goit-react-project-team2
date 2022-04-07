90b5714da1c968ab177c166b2e537f834e982c8e
Tate-T <tatjana.tarasovych@gmail.com>
2022-04-07 10:03:32 +0300

    delete excessive reducers & operations for add, get & delete at training

--- src/redux/training/trainingSlice.js
+++ src/redux/training/trainingSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { addBookToTraining, getTrainingList, deleteTrainingBook, startTraining } from './trainingOperatons';
+import { startTraining } from './trainingOperatons';
 
 const initialState = [{
     "name": "Test",
@@ -19,35 +19,28 @@ const initialState = [{
     "finishedReading": []
 }]
 
-// const initialState = {
-//     name: null,
-//     author: null,
-//     year: null,
-//     page: null,
-// };
-
 const trainingReducer = createSlice({
     name: 'training',
     initialState,
     extraReducers: {
-        [addBookToTraining.fulfilled](state, action) {
-            state.name = action.payload;
-            state.author = action.payload;
-            state.year = action.payload;
-            state.page = action.payload;
-        },
-        [getTrainingList.fulfilled](state, action) {
-            state.name = action.payload.name;
-            state.author = action.payload.author;
-            state.year = action.payload.year;
-            state.page = action.payload.page;
-        },
-        [deleteTrainingBook.fulfilled](state, action) {
-            state.name = null;
-            state.author = null;
-            state.year = null;
-            state.page = null;
-        },
+        // [addBookToTraining.fulfilled](state, action) {
+        //     state.name = action.payload;
+        //     state.author = action.payload;
+        //     state.year = action.payload;
+        //     state.page = action.payload;
+        // },
+        // [getTrainingList.fulfilled](state, action) {
+        //     state.name = action.payload.name;
+        //     state.author = action.payload.author;
+        //     state.year = action.payload.year;
+        //     state.page = action.payload.page;
+        // },
+        // [deleteTrainingBook.fulfilled](state, action) {
+        //     state.name = null;
+        //     state.author = null;
+        //     state.year = null;
+        //     state.page = null;
+        // },
         [startTraining.fulfilled](state, action) {
             state.name = action.payload.name;
             state.author = action.payload.author;
