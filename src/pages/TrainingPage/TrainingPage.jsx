279c01b9c3dc00cda6d56efb084b7e69ec372482
alxtpl <alxtpl@gmail.com>
2022-04-08 00:22:49 +0300

    adding timer and drop down

--- src/pages/TrainingPage/TrainingPage.jsx
+++ src/pages/TrainingPage/TrainingPage.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import MyTimer from '../../components/Timer/Timer';
+import DatePicker from '../../components/Datepicker/Datepicker';
 import Container from 'components/common/Container';
 import TrainingBookList from '../../components/TrainingBookList/TrainingBookList';
 import s from './TrainingPage.module.css';
 // import { addBookToTraining } from '../../redux/training/trainingOperatons';
 import { getBooks } from '../../redux/auth/authSelectors';
 import LineChart from 'components/LineChart/LineChart';
-
 
 const TrainingPage = () => {
   const books = useSelector(getBooks);
@@ -53,43 +54,74 @@ const TrainingPage = () => {
     setNewBook({ ...newBook, value });
     addBookToTraining();
   };
+  // {/* Code below belong to --TIMER-- Move it Move to statistics page */}
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 700); // 10 minutes timer
 
+  let timeend = new Date();
+  timeend = new Date(timeend.getFullYear() + 1, 0, 1);
+
+  //-------------------------------
+  //////////////datepickerfooUSetosendtoapi
+  const [trainingData, setTrainingData] = useState({
+    startDate: '',
+    endDate: '',
+    books: [],
+  });
+  const setStartDate = startDate => {
+    setTrainingData(prev => ({ ...prev, startDate }));
+  };
+  const setEndDate = endDate => {
+    setTrainingData(prev => ({ ...prev, endDate }));
+  };
+  //////////////////endofdatepickerfoo
   return (
-    // <Container>
-    <>
+    <Container>
       <div className={s.trainingPage}>
-        <div className={s.trainingContainer}>
-          <h2 className={s.trainingTitle}>Моє тренування</h2>
-          <form className={s.trainingChooseBook} onSubmit={onSubmit}>
-            <input
-              type="text"
-              name="book"
-              list="books"
-              placeholder="Обрати книги з бібліотеки"
-              className={s.trainingInput}
-              // onChange={handleInputChange}
-            />
-            <datalist id="books">
-              {books.map(book => (
-                <option value={book.title} key={book._id} />
-              ))}
-            </datalist>
-            <button
-              type="submit"
-              className={s.trainingBtn}
-              // onClick={addBookToTraining(book)}
-            >
-              Додати
-            </button>
-          </form>
-          <TrainingBookList newBooks={newBooks} />
+
+        <div className={s.trainingPageFlex}>
+          <div className={s.trainingContainer}>
+            <h2 className={s.trainingTitle}>Моє тренування</h2>
+            <DatePicker placeholder="Початок" setDate={setStartDate} />
+          <DatePicker placeholder="Завершення" setDate={setEndDate} />
+          <MyTimer expiryTimestamp={time} />
+            <MyTimer expiryTimestamp={timeend} />
+            
+            
+            <form className={s.trainingChooseBook} onSubmit={onSubmit}>
+              <input
+                type="text"
+                name="book"
+                list="books"
+                placeholder="Обрати книги з бібліотеки"
+                className={s.trainingInput}
+                // onChange={handleInputChange}
+              />
+              <datalist id="books">
+                {books.map(book => (
+                  <option value={book.title} key={book._id} />
+                ))}
+              </datalist>
+              <button
+                type="submit"
+                className={s.trainingBtn}
+                // onClick={addBookToTraining(book)}
+              >
+                Додати
+              </button>
+            </form>
+            <TrainingBookList newBooks={newBooks} />
+          </div>
+          <div className={s.trainingGoal}>
+            <h2 className={s.trainingTitle}>Моя мета прочитати</h2>
+          </div>
+
         </div>
-        <div className={s.trainingGoal}>
-          <h2 className={s.trainingTitle}>Моя мета прочитати</h2>
+        <div className={s.lineChart}>
+          <LineChart />
         </div>
       </div>
-      <LineChart />
-    </>
+    </Container>
   );
 };
 
