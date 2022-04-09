43690096cfd09defa518a4c4fc846ced58994d7e
Andrii Vinnykov <andrey.vinnikov@gmail.com>
2022-04-09 14:56:44 +0300

    Bugfix - remove redirect

--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -1,12 +1,16 @@
 import { useSelector } from "react-redux";
-import { Redirect, Route } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import { getIsAuth } from "../../redux/auth/authSelectors";
 
 const PublicRoute = ({ path, children }) => {
   const isAuth = useSelector(getIsAuth);
 
   return isAuth ? (
-    <Redirect to={"/library"} />
+    // <Redirect to={"/library"} />
+    <Switch>
+      <Route path={"/library"} />
+      <Route path={"/training"} />
+    </Switch>
   ) : (
     <Route path={path}>{children}</Route>
   );
